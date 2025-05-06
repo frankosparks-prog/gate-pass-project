4581ecde88e1fd85e6b6cc30a25cf58e2d1828d3
frankosparks-prog <mainafrank400@gmail.com>
2025-05-06 13:30:10 +0300

    Gate pass init..

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='text-3xl text-blue-500'>
+          Edit <code className="text-red-500">src/App.js</code> and save to reload.
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
